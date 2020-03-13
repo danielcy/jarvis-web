963b49cdb80f06d7ac80a9a7566d4349e1eb35cb
shuixiaoyang <shuixiaoyang@pinduoduo.com>
2020-03-13 16:39:57 +0800

    feat: fix port

--- src/ConfirmationPage.js
+++ src/ConfirmationPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./ConfirmationPage.css"
 
 // var host = "http://127.0.0.1:8000";
-var host = "http://118.25.41.14:8000";
+var host = "http://118.25.41.14:8080";
 
 class ConfirmationPage extends React.Component {
     constructor(props) {
